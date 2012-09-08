79739f50d704842df0a19e0a2995d534611ce29e
Chris Christoff <chriscct7@gmail.com>
2012-09-08 09:35:52 -0400

    BlackHawk 0.1.0 Urgent Hotfix: User table fix
    That was really dumb on my part. Should be commas there (no semicolons
    in the SQL).

--- Login/setup/index.php
+++ Login/setup/index.php
@@ -131,8 +131,8 @@ $mysql_users = 'CREATE TABLE IF NOT EXISTS `users` (
   `password` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `date` date NOT NULL,
-  `loginrole` int(11) NOT NULL;
-  `garagerole` int(11) NOT NULL;
+  `loginrole` int(11) NOT NULL,
+  `garagerole` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=0 ;';
 
