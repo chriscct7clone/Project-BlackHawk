fdc0c5dc842373bc3392a7ab94106c20eefccb21
Chris Christoff <chriscct7@gmail.com>
2012-09-07 22:45:38 -0400

    Travis-CI upgrade makes echo's echo in build output

--- travis.php
+++ travis.php
@@ -14,5 +14,5 @@ else{ // On another planet o.O
 $status='God Save the Queen';
 var_dump($status); //To get results in Travis CI, you must use PHP's var_dump() function
 }
-echo '<h1>Hi There LocalHost Testers!</h1>'; // Will not print in Travis-CI
+echo '<h1>Hi There LocalHost Testers!</h1>'; // assertFalse() on Travis-CI upgrade
 ?>
