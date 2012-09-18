6ac17add5ca834e38093a198202489301c84b61d
Chris Christoff <chriscct7@gmail.com>
2012-09-18 19:53:42 -0400

    0.2.0 RC4 Preview Ed

--- assets/garage.class.php
+++ assets/garage.class.php
@@ -2,19 +2,20 @@
 /*
  * Garage Class
  */
-class Config {
+class Garage {
 	// no other garage in use
-	public function installgaragetable($name, $rolesallowed, ){
+	public function installgaragetable($name, $rolesallowed,$floors){
 		require_once('config.inc.php');
 		require_once("database.class.php");
 		/* Start an instance of the Database Class */
 		$database = new database();
 		//TODO: STRreplace name
 		$namefordb=$name;
-		// insert name into the garages_list
 		
+		// TODO: insert name into the garages_list
 		
-		$mysql_garage_list = 'CREATE TABLE IF NOT EXISTS `$namefordb` (
+		// create garage table
+		$mysql_garage = 'CREATE TABLE IF NOT EXISTS `$namefordb` (
 							`floor` int(3) NOT NULL,
 							`spot` int(3) NOT NULL,
 							`status` int(3) NOT NULL,
@@ -24,19 +25,91 @@ class Config {
 							PRIMARY KEY (`floor`)
 							) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=0 ;';
 		}
-	$statement = $pdo->prepare($mysql_garage_list);
-		if($statement->execute()){
-			$notice->add('success', $name.' added!');
-		} else {
-			$notice->add('error',  $name.' not added!');
-		}
+		$statement = $pdo->prepare($mysql_garage);
+			if($statement->execute()){
+				$notice->add('success', $name.' was added!');
+			} else {
+				$notice->add('error',  $name.' was not added!');
+			}
+
+/* 
+Intake Array format:
+
+$floors = array(
+    1 => array( // floor 0 is ground floor...not garage
+	        'num_spots' => 100, 
+            'police' => array(15,16,17), //for ground level parking  
+			'num_police' => 3,
+			'teacher' => array (47,62), //for ground level parking  
+			'num_teacher' => 2,
+			'visitors' => array (47,62), //for ground level parking  
+			'num_visitors' => 2,
+            'handicap' => array(15,16,17),
+			'num_handicap' => 3,
+			'motorcycle' => array (47,62),
+			'num_motorcycle' => 2
+        ),   
+
+    2 => array(
+	        'num_spots' => 100,
+            'police' => array(15,16,17),  
+			'num_police' => 3,
+			'teacher' => array (47,62),
+			'num_teacher' => 2,
+			'visitors' => array (47,62),
+			'num_visitors' => 2,
+            'handicap' => array(15,16,17),
+			'num_handicap' => 3,
+			'motorcycle' => array (47,62),
+			'num_motorcycle' => 2
+        )
+);
+
+*/
+$numfloors=$count($floors);
+
+for ($a=0, $a>$numfloors,$a++){
+$numspots=[$a]['num_spots'][0];
+for ($b=0, $b>$numspots, $b++){
+    $floor=$a;
+	$spot=$b+1;
+	$type=0; // Student
+	// Check if in Police
+	for ($x=0,$x>($floors[$a]['num_police']),$x++){
+		if ($spot==$floors[$a]['police'][$x]){
+			$type=1;
+		}	
+	}
+	// Check if in teacher
+	for ($x=0,$x>($floors[$a]['num_teacher']),$x++){
+		if ($spot==$floors[$a]['teacher'][$x]){
+			$type=2;
+		}	
+	}		
+	// Check if in Handicap
+	for ($x=0,$x>($floors[$a]['num_handicap']),$x++){
+		if ($spot==$floors[$a]['handicap'][$x]){
+			$type=3;
+		}	
+	}
+	// Check if in motorcycle
+	for ($x=0,$x>($floors[$a]['num_motorcycle']),$x++){
+		if ($spot==$floors[$a]['motorcycle'][$x]){
+			$type=4;
+		}	
+	}
+	// TODO: Reserved spots. Type 5 and pass uid along for the ride.
 	$date = date("Y-m-d", time());
-	// Loop for each spot
 	$uid=null; //no one's parked there
-	$status=0; //open
+	$status=0; //open	
 	$database->query('INSERT INTO $namefordb(floor, spot, status, uid, type, time) VALUES(:floor, :spot, :status, :uid, :type,:time)', array(':floor' => $floor, ':spot' => $spot,':status' => $status, ':uid' => $uid,':type' => $type,':time' => $time  ));
-	public function addgarage($name) {
+	}
+	}
+}
+	public function addgarage($name, $rolesallowed, $floors) {
 	
+	
+	installgaragetable($name, $rolesallowed,$floors); // Adds database
  	}
 	public function editgarage($name) {
  	}
