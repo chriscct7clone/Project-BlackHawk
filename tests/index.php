27565481bf43664ac33fd68294274f560924282d
Chris Christoff <chriscct7@gmail.com>
2012-09-25 12:05:42 -0400

    Fixes index pages with DocBlocks

--- tests/index.php
+++ tests/index.php
@@ -1,3 +1,19 @@
 <?php
-// echo("Silence is golden");
+ /**
+ * Security Index Page
+ *
+ * This file prevents directory listing
+ *
+ * PHP version 5.2.17 or higher
+ *
+ * LICENSE: TBD
+ *
+ * @package    BlackHawk
+ * @subpackage Security
+ * @author     Chris Christoff <chris@futuregencode.com>
+ * @copyright  2012 Project BlackHawk
+ * @license    http://www.futuregencode.com/blackhawk/404  License 1.00
+ * @version    0.3.0
+ * @since      File available since Release 0.3.0
+ */
 ?>
