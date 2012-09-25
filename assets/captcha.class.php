80d809db294f94d781d2f776fe3bb3dbebf0f0f2
Chris Christoff <chriscct7@gmail.com>
2012-09-25 10:48:53 -0400

    finished Captcha docs

--- assets/captcha.class.php
+++ assets/captcha.class.php
@@ -1,76 +1,35 @@
 <?php
  /**
- * Short description for file
+ * Creates Captcha Image
  *
- * Long description for file (if any)...
+ * This file creates the image for the captcha, as long as Captcha has been turned on during setup.
  *
- * PHP version 5
+ * PHP version 5.2.17 or higher
  *
- * LICENSE: This source file is subject to version 3.01 of the PHP license
- * that is available through the world-wide-web at the following URI:
- * http://www.php.net/license/3_01.txt.  If you did not receive a copy of
- * the PHP License and are unable to obtain it through the web, please
- * send a note to license@php.net so we can mail you a copy immediately.
+ * LICENSE: TBD
  *
- * @category   CategoryName
- * @package    PackageName
- * @author     Original Author <author@example.com>
- * @author     Another Author <another@example.com>
- * @copyright  1997-2005 The PHP Group
- * @license    http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @package    BlackHawk
+ * @author     Chris Christoff <chris@futuregencode.com>
+ * @copyright  2012 Project BlackHawk
+ * @license    http://www.futuregencode.com/blackhawk/404  License 1.00
  * @version    SVN: $Id$
  * @link       http://pear.php.net/package/PackageName
- * @see        NetOther, Net_Sample::Net_Sample()
- * @since      File available since Release 1.2.0
- * @deprecated File deprecated in Release 2.0.0
+ * @see        member()
+ * @since      File available since Release 0.3.0
  */
 
  
  /**
- * An example of how to write code to PEAR's standards
+ * Implements the captcha class
  *
- * Docblock comments start with "/**" at the top.  Notice how the "/"
- * lines up with the normal indenting and the asterisks on subsequent rows
- * are in line with the first asterisk.  The last line of comment text
- * should be immediately followed on the next line by the closing asterisk
- * and slash and then the item you are commenting on should be on the next
- * line below that.  Don't add extra lines.  Please put a blank line
- * between paragraphs as well as between the end of the description and
- * the start of the @tags.  Wrap comments before 80 columns in order to
- * ease readability for a wide variety of users.
- *
- * Docblocks can only be used for programming constructs which allow them
- * (classes, properties, methods, defines, includes, globals).  See the
- * phpDocumentor documentation for more information.
- * http://phpdoc.org/docs/HTMLSmartyConverter/default/phpDocumentor/tutorial_phpDocumentor.howto.pkg.html
- *
- * The Javadoc Style Guide is an excellent resource for figuring out
- * how to say what needs to be said in docblock comments.  Much of what is
- * written here is a summary of what is found there, though there are some
- * cases where what's said here overrides what is said there.
- * http://java.sun.com/j2se/javadoc/writingdoccomments/index.html#styleguide
- *
- * The first line of any docblock is the summary.  Make them one short
- * sentence, without a period at the end.  Summaries for classes, properties
- * and constants should omit the subject and simply state the object,
- * because they are describing things rather than actions or behaviors.
- *
- * Below are the tags commonly used for classes. @category through @version
- * are required.  The remainder should only be used when necessary.
- * Please use them in the order they appear here.  phpDocumentor has
- * several other tags available, feel free to use them.
- *
- * @category   CategoryName
- * @package    PackageName
- * @author     Original Author <author@example.com>
- * @author     Another Author <another@example.com>
- * @copyright  1997-2005 The PHP Group
- * @license    http://www.php.net/license/3_01.txt  PHP License 3.01
- * @version    Release: @package_version@
+ * @package    BlackHawk
+ * @author     Chris Christoff <chris@futuregencode.com>
+ * @copyright  2012 Project BlackHawk
+ * @license    http://www.futuregencode.com/blackhawk/404  License 1.00
+ * @version    SVN: $Id$
  * @link       http://pear.php.net/package/PackageName
- * @see        NetOther, Net_Sample::Net_Sample()
- * @since      Class available since Release 1.2.0
- * @deprecated Class deprecated in Release 2.0.0
+ * @see        member()
+ * @since      File available since Release 0.3.0
  */
 class captcha {
 /**
