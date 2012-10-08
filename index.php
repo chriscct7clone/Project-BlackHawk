5517293754ea049e497aa7f6bda2132e81bee53f
Chris Christoff <chriscct7@gmail.com>
2012-10-08 05:46:04 -0400

    Fix link on homepage

--- index.php
+++ index.php
@@ -37,12 +37,13 @@ if($action == 'secure') {
 	$member->LoggedIn();
 	$title   = 'Redirecting';
 	// $role    = $member->role(); Gets role of user
-	$content =  '<meta http-equiv="refresh" content="2;url=/role/student_campus.php" /><p>Redirecting....</p>';
+	$content =  '<meta http-equiv="refresh" content="2;url=roles/student_campus.php" /><p>Redirecting....</p>';
 	}
-if($action == 'global') {
+else if($action == 'global') {
+	$member->LoggedIn();
 	$title   = 'Redirecting';
-	$content =  '<meta http-equiv="refresh" content="2;url=global.php" /><p>Loading Statistics....</p>';
-} else {
+	$content =  '<meta http-equiv="refresh" content="2;url=statistics/global.php" /><p>Loading Statistics....</p>';
+} else{
 	$title   = 'Welcome';
 	$content =  'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.';
 }
