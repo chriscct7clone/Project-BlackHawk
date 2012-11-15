eebff975390918176fcbf4f05612dd5c2058a7e5
James Stephens <StephensJ7@yahoo.com>
2012-11-15 10:07:42 -0500

    First commit

---
+++ tests/MembersClassTest.php
@@ -0,0 +1,22 @@
+<?php
+require_once('assets/members.inc.php');
+
+class MembersClassTest extends PHPUnit_Framework_TestCase
+{
+	private $pdo;
+    public function setUp()
+    {
+        $database->query("CREATE TABLE hello (what VARCHAR(50) NOT NULL)");
+    }
+
+    public function tearDown()
+    {
+        $this->pdo->query("DROP TABLE hello");
+    }
+
+	public function checkTable()
+	{
+		$result = $database->query("SELECT * FROM hello");
+		assertTrue($result);
+	}
+}
