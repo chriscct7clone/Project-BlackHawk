ae5d732b4c0cf375e68ec1c50bc465427b343628
Chris Christoff <chriscct7@gmail.com>
2012-11-26 15:00:51 -0500

    Travis only

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,10 @@
 <?php
 error_reporting(E_ALL | E_STRICT);
+$file = 'config.inc.php';
+$newfile = '../assets/config.inc.php';
+if (!copy($file, $newfile)) {
+    echo "failed to copy $file...\n";
+}
 function loader($class)
 {
     $file = $class . '.php';
