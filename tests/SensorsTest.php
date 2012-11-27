7b978f94a5314497d7410300dfc523f713a895b9
Chris Christoff <chriscct7@gmail.com>
2012-11-26 21:03:16 -0500

    Blocks

--- tests/SensorsTest.php
+++ tests/SensorsTest.php
@@ -3,30 +3,30 @@
 * @backupGlobals disabled
 * @backupStaticAttributes disabled
 */
-class SensorsTest extends PHPUnit_Framework_TestCase {
+// class SensorsTest extends PHPUnit_Framework_TestCase {
 
-    /**
-     * @var Sensors
-     */
-    protected $object;
+    // /**
+     // * @var Sensors
+     // */
+    // protected $object;
 
-    /**
-     * Sets up the fixture, for example, opens a network connection.
-     * This method is called before a test is executed.
-     */
-    protected function setUp() {
-        $this->object = new Sensors;
-    }
+    // /**
+     // * Sets up the fixture, for example, opens a network connection.
+     // * This method is called before a test is executed.
+     // */
+    // protected function setUp() {
+        // $this->object = new Sensors;
+    // }
 
-    /**
-     * Tears down the fixture, for example, closes a network connection.
-     * This method is called after a test is executed.
-     */
-    protected function tearDown() {
+    // /**
+     // * Tears down the fixture, for example, closes a network connection.
+     // * This method is called after a test is executed.
+     // */
+    // protected function tearDown() {
         
-    }
-	public function testIncomplete(){
-		 // Remove the following lines when you implement this test.
-        $this->markTestIncomplete('This test has not been implemented yet.');
-	}
-}
+    // }
+	// public function testIncomplete(){
+		 // // Remove the following lines when you implement this test.
+        // $this->markTestIncomplete('This test has not been implemented yet.');
+	// }
+// }
--- tests/UPDTest.php
+++ tests/UPDTest.php
@@ -3,31 +3,31 @@
 * @backupGlobals disabled
 * @backupStaticAttributes disabled
 */
-class UPDTest extends PHPUnit_Framework_TestCase {
+// class UPDTest extends PHPUnit_Framework_TestCase {
 
-    /**
-     * @var UPD
-     */
-    protected $object;
+    // /**
+     // * @var UPD
+     // */
+    // protected $object;
 
-    /**
-     * Sets up the fixture, for example, opens a network connection.
-     * This method is called before a test is executed.
-     */
-    protected function setUp() {
-        $this->object = new UPD;
-    }
+    // /**
+     // * Sets up the fixture, for example, opens a network connection.
+     // * This method is called before a test is executed.
+     // */
+    // protected function setUp() {
+        // $this->object = new UPD;
+    // }
 
-    /**
-     * Tears down the fixture, for example, closes a network connection.
-     * This method is called after a test is executed.
-     */
-    protected function tearDown() {
+    // /**
+     // * Tears down the fixture, for example, closes a network connection.
+     // * This method is called after a test is executed.
+     // */
+    // protected function tearDown() {
         
-    }
-	public function testIncomplete(){
-		 // Remove the following lines when you implement this test.
-        $this->markTestIncomplete('This test has not been implemented yet.');
-	}
+    // }
+	// public function testIncomplete(){
+		 // // Remove the following lines when you implement this test.
+        // $this->markTestIncomplete('This test has not been implemented yet.');
+	// }
 
-}
+// }
